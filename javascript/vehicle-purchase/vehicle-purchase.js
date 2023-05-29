1147eb8114dd36b436477331fb7fe208a75d133e
preto-phil <philippretorius.79@gmail.com>
2023-05-29 21:12:57 +0200

    Vehicle exercise completed

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,11 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Please implement the needsLicense function');
+  if (kind === 'car' || kind === 'truck') {
+    return true;
+  } else {
+    return false;
+  };
 }
 
 /**
@@ -23,7 +27,11 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Please implement the chooseVehicle function');
+  if (option1 < option2 == true) {
+    return option1 + ' is clearly the better choice.';
+  } else {
+    return option2 + ' is clearly the better choice.';
+  };
 }
 
 /**
@@ -35,5 +43,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+  if (age < 3) {
+    return originalPrice * 0.8;
+  } else if (age >= 3 && age <= 10) {
+    return originalPrice * 0.7;
+  } else {
+    return originalPrice * 0.5;
+  };
 }
