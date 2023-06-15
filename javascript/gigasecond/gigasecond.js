f389c43c95a3ac0dbd7fd62baadac7ab32f9825b
preto-phil <philippretorius.79@gmail.com>
2023-06-15 23:26:22 +0200

    Gigasecond complete

--- javascript/gigasecond/gigasecond.js
+++ javascript/gigasecond/gigasecond.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  const dateCopy = new Date(date);
+  dateCopy.setSeconds(dateCopy.getSeconds() + 1000000000);
+  return dateCopy;
 };
